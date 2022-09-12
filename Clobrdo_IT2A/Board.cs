6264c4d1616f703c8972fa499482716d87604c7e
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-09-12 13:47:25 +0200

    Add project files.

---
+++ Clobrdo_IT2A/Board.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobrdo_IT2A
+{
+    public class Board
+    {
+        private Figure[] path;
+        private Figure[] color1Home;
+        private Figure[] color2Home;
+        private Figure[] color3Home;
+        private Figure[] color4Home;
+
+        public int Color1Start { get; }
+        public int Color2Start { get; }
+        public int Color3Start { get; }
+        public int Color4Start { get; }
+
+        public Board()
+        {
+            path = new Figure[40];
+            color1Home = new Figure[4];
+            color2Home = new Figure[4];
+            color3Home = new Figure[4];
+            color4Home = new Figure[4];
+            Color1Start = 0;
+            Color2Start = 10;
+            Color3Start = 20;
+            Color4Start = 30;
+        }
+    }
+}
---
+++ Clobrdo_IT2A/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobrdo_IT2A
+{
+    public class Figure
+    {
+        public FigureColor Color { get; }
+
+        public Figure(FigureColor color)
+        {
+            Color = color;
+        }
+    }
+
+    public enum FigureColor
+    {
+        Color1,
+        Color2,
+        Color3,
+        Color4
+    }
+}
