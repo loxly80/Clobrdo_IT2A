876811123a0ee3a1481be63927d3dd2d47d34600
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2022-09-26 13:45:37 +0200

    nachystané třídy

---
+++ Clobrdo_IT2A/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobrdo_IT2A
+{
+    public class Game
+    {
+        public Board Board { get; }
+        public Player[] Players { get; }
+        public Dice Dice { get; }
+
+        public Game(int playersCount)
+        {
+            if (playersCount <= 0)
+            {
+                throw new ArgumentException("Počet hráčů musí být alespoň 1");
+            }
+            if (playersCount > 4)
+            {
+                throw new ArgumentException("Počet hráčů musí být maximálně 4");
+            }
+            Players = new Player[playersCount];
+            for(int i = 0; i < Players.Length; i++)
+            {
+                Players[i] = new Player((FigureColor)i);
+            }
+            Dice = new Dice();
+            Board = new Board();
+        }
+    }
+}
---
+++ Clobrdo_IT2A/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clobrdo_IT2A
+{
+    public class Player
+    {
+        public Figure[] Figures { get; set; }
+
+        public Player(FigureColor figureColor)
+        {
+            Figures = new Figure[4];
+            for (int i = 0; i < Figures.Length; i++)
+            {
+                Figures[i] = new Figure(figureColor);
+            }
+        }
+    }
+}
